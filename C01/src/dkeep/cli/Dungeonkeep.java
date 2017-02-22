ed6942131187e2faee02b7052e5d5aa1beb66fa9
luisnmartins <luis.n.martins97@gmail.com>
2017-02-22 17:07:32 +0000

    added cli and logic packages

---
+++ C01/src/dkeep/cli/Dungeonkeep.java
@@ -0,0 +1,6 @@
+package dkeep.cli;
+
+public class Dungeonkeep {
+
+	
+}
---
+++ C01/src/dkeep/logic/Character.java
@@ -0,0 +1,11 @@
+package dkeep.logic;
+
+public class Character {
+
+	private int x; //matrix line counting from 0 
+	private int y; //matrix column counting from 0 
+	private char element; //char that represents the character on the map
+	
+	
+	
+}
