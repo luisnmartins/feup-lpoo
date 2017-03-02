6b5de1dd8bdf4dfb370b2d5f6e29b702348ce89b
luisnmartins <luis.n.martins97@gmail.com>
2017-03-02 16:20:03 +0000

    test package created - Finish class 2/3/2017

---
+++ C02/src/dkeep/test/TestDungeonGameLogic.java
@@ -0,0 +1,22 @@
+package dkeep.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+//import dkeep.logic.CellPosition;
+
+public class TestDungeonGameLogic {
+
+	char[][] map = {{'X', 'X', 'X', 'X', 'X'},
+					{'X', 'H', ' ', 'G', 'X'},
+					{'I', ' ', ' ', ' ', 'X'},
+					{'I', 'k', ' ', ' ', 'X'},
+					{'X', 'X', 'X', 'X', 'X'}};
+	
+	@Test
+	public void TestMoveHeroIntoToFreeCell()
+	{
+		
+	}
+	
+}
