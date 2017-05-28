9be1d7715afc2ec0f4edc41ebad98833d3e5c673
Luís Martins <luis.n.martins97@gmail.com>
2017-05-28 09:14:54 +0100

    start suggestions

---
+++ PocketSave/app/src/main/java/lpoo/pocketsave/Logic/Suggestions.java
@@ -0,0 +1,27 @@
+package lpoo.pocketsave.Logic;
+
+
+public class Suggestions {
+
+    //CORRENTE
+
+    //TODO: veriicar se uma categoria esta acima (30%) do seu valor previsto comparativamente com o que ja passou do mes corrente
+
+
+    //TODO: Verificar o metodo de pagamaneto dominante e se for dinheiro verificar seo valor medio das transacoes
+        // e de baixa quantia (<20€) - para categorias que estao muito proximo do limite
+
+
+
+    //INICIO MES
+
+    //TODO: comparacao do ultimo mes para cada categoria verificando as que ficaram a baixo e as que ficaram a cima do valor previsto
+
+    //TODO: verificar valores medios para os valores previstos e valores efetivos para cada categoria desde o inicio dos registos
+
+    //TODO: sugestao dos valores aproximados para o mes seguinte tendo em conta a variacao em termos de receitas
+    // e de despesas fixas e mantendo a mesma percentagem de poupanca do mes passado
+
+
+
+}
