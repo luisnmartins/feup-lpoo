3c1f42f8a9855560746e54398bed98649c6f77fc
Luís Martins <luis.n.martins97@gmail.com>
2017-06-11 20:31:36 +0100

    Update

--- PocketSave/app/src/main/java/lpoo/pocketsave/View/ChooseCategoriesForMain.java
+++ PocketSave/app/src/main/java/lpoo/pocketsave/View/ChooseCategoriesForMain.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -63,8 +64,9 @@ public class ChooseCategoriesForMain extends DialogFragment{
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
 
-                        if(count == -1)
+                        if(count == 5) {
                             setThings();
+                        }
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
